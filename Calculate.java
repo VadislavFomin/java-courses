089de64deaa0371431d8677bce1d90ffc0b685b9
Vlad <vladfomin2008@gmail.com>
2020-04-09 21:50:02 +0300

    upgrade Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,20 @@
 public class Calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
+
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+
+		int sum = first + second;
+		int mult = first * second;
+		int power = first^second;
+		int priv = first / second;
+
+		System.out.println("Sum " + sum);
+		System.out.println("Mult " + mult);
+		System.out.println("Power " + power);
+		System.out.println("Priv " + priv);
+		
+
 	}
 }
