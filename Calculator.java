19af38b5f2cbe23f5cfa671980b03b4788b5081f
Vlad <vladfomin2008@gmail.com>
2020-04-10 19:47:17 +0300

    add operations: -,*,/,^

--- Calculator.java
+++ Calculator.java
@@ -16,8 +16,34 @@ public class Calculator {
 			this.result += param;
 		}
 	}
-	
-	
+	/**
+		Опреация вычитания
+		@param param1, param2 аргументы разности
+	*/
+	public void subtract (int param1, int param2){	
+			this.result = param1 - param2;
+	}
+	/**
+		Опреация умножения
+		@param param1, param2 аргументы произведения
+	*/
+	public void multipl (int param1, int param2){	
+			this.result = param1 * param2;
+	}
+	/**
+		Опреация деления
+		@param param1, param2 аргументы частного
+	*/
+	public void division (int param1, int param2){	
+			this.result = param1 / param2;
+	}
+	/**
+		Опреация степени
+		@param param1, param2 аргументы степени
+	*/
+	public void power (int param1, int param2){	
+			this.result = (int) Math.pow(param1, param2);
+	}
 	/**
 		Получить результат выражения
 		@return результат выражения
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InteractRunner {
@@ -7,20 +9,62 @@ public class InteractRunner {
 		try {
 			Calculator calc = new Calculator();
 			String exit = "no";
-
+			ArrayList<String> list;
+			
 			while(!exit.equals("yes")){
-				System.out.println("Enter first number:");
-				String firstNum = reader.next();
-				System.out.println("Enter second number:");
-				String secondNum = reader.next();
-				calc.add(Integer.valueOf(firstNum), Integer.valueOf(secondNum));
-				System.out.println("Result:" + calc.getResult());
-				System.out.println("Exit: yes/no");
-				exit = reader.next();
+				System.out.println("Choose operation: +, -, *, /, ^");
+				String operation = reader.next();
+				switch (operation) {
+				case "+":
+					list = (ArrayList<String>) enterArgs(reader); 
+					calc.add(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
+					exit = showResult(reader, calc, exit);
+					break;
+				case "-":
+					list = (ArrayList<String>) enterArgs(reader); 
+					calc.subtract(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
+					exit = showResult(reader, calc, exit);
+					break;
+				case "^":
+					list = (ArrayList<String>) enterArgs(reader); 
+					calc.power(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
+					exit = showResult(reader, calc, exit);
+					break;
+				case "*":
+					list = (ArrayList<String>) enterArgs(reader); 
+					calc.multipl(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
+					exit = showResult(reader, calc, exit);
+					break;
+				case "/":
+					list = (ArrayList<String>) enterArgs(reader); 
+					calc.division(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
+					exit = showResult(reader, calc, exit);
+					break;
+				default:
+					System.out.println("You entered wrong operation");
+					break;
+				}
 			}
 		} finally {
 			reader.close();
 		}
 		
+	}
+	private static List<String> enterArgs(Scanner reader) {
+		List<String> list = new ArrayList<>();
+		
+		System.out.println("Enter first number:");
+		String firstNum = reader.next();
+		list.add(firstNum);
+		System.out.println("Enter second number:");
+		String secondNum = reader.next();
+		list.add(secondNum);
+		return list;
+	}
+	
+	private static String showResult(Scanner reader, Calculator calc, String exit) {
+		System.out.println("Result:" + calc.getResult());
+		System.out.println("Exit: yes/no");
+		return exit = reader.next();
 	}
 }
