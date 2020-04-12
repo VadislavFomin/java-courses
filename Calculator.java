859da763ccfe7bcece8901a49df2cf141793d009
Vlad <vladfomin2008@gmail.com>
2020-04-12 18:58:07 +0300

    Added the ability to use the result of the previous calculation

--- Calculator.java
+++ Calculator.java
@@ -1,54 +1,61 @@
+
 /**
 	класс реализует калькулятор
 */
 public class Calculator {
+	public Calculator() {
+		super();
+		
+	}
 	/**
 		результат вычислений
 	*/
-	private int result;
+	private float result;
 	
 	/**
 		Опреация сложения
 		@param params аргументы суммирования
 	*/
-	public void add(int ...params){
-		for (Integer param: params) {
-			this.result += param;
-		}
+	public void add(int param1, int param2){
+		this.result = (param1 + param2);
 	}
-	/**
-		Опреация вычитания
-		@param param1, param2 аргументы разности
-	*/
+	
+	/**Опреация вычитания
+	 * @param param1 первое число
+	 * @param param2 второе число
+	 */
 	public void subtract (int param1, int param2){	
-			this.result = param1 - param2;
+			this.result = (param1 - param2);
 	}
 	/**
 		Опреация умножения
-		@param param1, param2 аргументы произведения
+	* @param param1 первое число
+	* @param param2 второе число
 	*/
 	public void multipl (int param1, int param2){	
 			this.result = param1 * param2;
 	}
 	/**
 		Опреация деления
-		@param param1, param2 аргументы частного
+	* @param param1 первое число
+	* @param param2 второе число
 	*/
 	public void division (int param1, int param2){	
-			this.result = param1 / param2;
+			this.result = ((float)param1 / (float)param2);
 	}
 	/**
 		Опреация степени
-		@param param1, param2 аргументы степени
-	*/
+	 * @param param1 первое число
+	 * @param param2 второе число
+	 */
 	public void power (int param1, int param2){	
-			this.result = (int) Math.pow(param1, param2);
+			this.result = (float) Math.pow(param1, param2);
 	}
 	/**
 		Получить результат выражения
 		@return результат выражения
 	*/
-	public int getResult(){
+	public float getResult(){
 		return this.result;
 	}
 	
--- InteractRunner.java
+++ InteractRunner.java
@@ -3,42 +3,55 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InteractRunner {
-	public static void main(String[] args){
-		Scanner reader = new Scanner(System.in);
+
+	public InteractRunner() {
+		super();
+	}
+
+	/**
+	 * сканер для обработкии ввода пользователя
+	 */
+	private static Scanner		reader		= new Scanner(System.in);
+	private static Calculator	calc		= new Calculator();
+	private static String		clearResult	= "yes";
+
+	public static void main(String[] args) {
 
 		try {
-			Calculator calc = new Calculator();
-			String exit = "no";
-			ArrayList<String> list;
-			
-			while(!exit.equals("yes")){
+			/**
+			 * Хранит ответ пользователя на вопрос о выходе из приложения: yes / no
+			 */
+			String			exit	= "no";
+			List<Integer>	list;
+
+			while (!exit.equals("yes")) {
 				System.out.println("Choose operation: +, -, *, /, ^");
 				String operation = reader.next();
 				switch (operation) {
 				case "+":
-					list = (ArrayList<String>) enterArgs(reader); 
-					calc.add(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-					exit = showResult(reader, calc, exit);
+					list = (ArrayList<Integer>) enterArgs(reader);
+					calc.add(list.get(0), list.get(1));
+					exit = showResult(reader);
 					break;
 				case "-":
-					list = (ArrayList<String>) enterArgs(reader); 
-					calc.subtract(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-					exit = showResult(reader, calc, exit);
+					list = (ArrayList<Integer>) enterArgs(reader);
+					calc.subtract(list.get(0), list.get(1));
+					exit = showResult(reader);
 					break;
 				case "^":
-					list = (ArrayList<String>) enterArgs(reader); 
-					calc.power(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-					exit = showResult(reader, calc, exit);
+					list = (ArrayList<Integer>) enterArgs(reader);
+					calc.power(list.get(0), list.get(1));
+					exit = showResult(reader);
 					break;
 				case "*":
-					list = (ArrayList<String>) enterArgs(reader); 
-					calc.multipl(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-					exit = showResult(reader, calc, exit);
+					list = (ArrayList<Integer>) enterArgs(reader);
+					calc.multipl(list.get(0), list.get(1));
+					exit = showResult(reader);
 					break;
 				case "/":
-					list = (ArrayList<String>) enterArgs(reader); 
-					calc.division(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-					exit = showResult(reader, calc, exit);
+					list = (ArrayList<Integer>) enterArgs(reader);
+					calc.division(list.get(0), list.get(1));
+					exit = showResult(reader);
 					break;
 				default:
 					System.out.println("You entered wrong operation");
@@ -48,23 +61,88 @@ public class InteractRunner {
 		} finally {
 			reader.close();
 		}
-		
+
 	}
-	private static List<String> enterArgs(Scanner reader) {
-		List<String> list = new ArrayList<>();
-		
-		System.out.println("Enter first number:");
-		String firstNum = reader.next();
-		list.add(firstNum);
+
+	/**
+	 * Запрос пользователю на ввод двух чисел в коммандную строку. Валидация чисел
+	 * на Integer
+	 * 
+	 * @param reader сканер для обработки ввода пользователя
+	 * @return ArrayList<Integer> список с числами
+	 */
+	private static List<Integer> enterArgs(Scanner reader) {
+		List<Integer> list = new ArrayList<>();
+
+		// Если предыдущий результат надо поместить в первый аргумент
+		if (clearResult.equals("no")) {
+			int firstNum = (int) calc.getResult();
+			list.add(firstNum);
+			
+		// Если это первая операция или предыдущий результат был обнулен
+		} else if (clearResult.equals("yes")) {
+			System.out.println("Enter first number:");
+			boolean check = false;
+			while (!check) {
+				try {
+					int firstNum = Integer.parseInt(reader.next());
+					list.add(firstNum);
+					check = true;
+				} catch (NumberFormatException e) {
+					e.getMessage();
+					System.out.println("You entered wrong Number.class Try again.");
+				}
+			}
+		}
+
 		System.out.println("Enter second number:");
-		String secondNum = reader.next();
-		list.add(secondNum);
+		boolean check = false;
+		while (!check) {
+			try {
+				int secondNum = Integer.parseInt(reader.next());
+				list.add(secondNum);
+				check = true;
+			} catch (NumberFormatException e) {
+				e.getMessage();
+				System.out.println("You entered wrong Number. Try again.");
+			}
+		}
 		return list;
 	}
-	
-	private static String showResult(Scanner reader, Calculator calc, String exit) {
-		System.out.println("Result:" + calc.getResult());
+
+	/**
+	 * Отображение результата вычисления. Запрос на выход из приложения. Да -
+	 * Очистка результата вычисления Нет - запрос на очистку результата
+	 * 
+	 * @param reader  - сканер для обработки ввода пользователя
+	 * @param calcul- объект калькулятора
+	 * @return exit- ответ пользователя: yes/no
+	 */
+	private static String showResult(Scanner reader) {
+		System.out.printf("Result: %.2f %n", calc.getResult());
+
 		System.out.println("Exit: yes/no");
-		return exit = reader.next();
+		String exit = reader.next();
+		
+		if (exit.equals("yes")) {
+			calc.clearResult();
+		} else {
+			
+			System.out.println("Does clear result?: yes/no");
+			clearResult = reader.next();
+			while (!clearResult.equals("yes") || !clearResult.equals("no")) {
+				if (clearResult.equals("yes")) {
+					calc.clearResult();
+					break;
+				} else if(clearResult.equals("no")) {
+					break;
+				} else {
+					System.out.println("You mistaked. Does clear result?: yes/no");
+					clearResult = reader.next();
+				}
+			}
+			
+		}
+		return exit;
 	}
 }
